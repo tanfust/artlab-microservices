9d84fec4a318a7315a4587884d1bee651a955e15
Wassim Ben R <wassimbenr@gmail.com>
2023-10-24 18:03:55 +0100

    all services

--- Api_Gateway_server/src/main/java/com/example/api_gateway_server/SpringCloudConfig.java
+++ Api_Gateway_server/src/main/java/com/example/api_gateway_server/SpringCloudConfig.java
@@ -4,20 +4,36 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 @Configuration
 public class SpringCloudConfig {
-
-    @Bean
-    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
-        return builder.routes()
-                .route(r -> r.path("/users/**")
-                        .uri("http://localhost:3000/"))
-                .route(r -> r.path("/Article/**")
-                        .uri("http://localhost:8001/"))
-                .route(r -> r.path("/auction/**")
-                        .uri("http://localhost:8002/"))
-                .build();
-       
-    }
+//    @Bean
+//    public CorsWebFilter corsWebFilter() {
+//        CorsConfiguration corsConfig = new CorsConfiguration();
+//        corsConfig.addAllowedOrigin("http://localhost:8081");
+//        corsConfig.addAllowedMethod("*");
+//        corsConfig.addAllowedHeader("*");
+//
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", corsConfig);
+//
+//        return new CorsWebFilter(source);
+//    }
+//
+//    @Bean
+//    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+//        return builder.routes()
+//                .route(r -> r.path("/users/**")
+//                        .uri("http://localhost:3000/")
+//
+//                )
+//                .route(r -> r.path("/Article/**")
+//                        .uri("http://localhost:8001/"))
+//                .route(r -> r.path("/auction/**")
+//                        .uri("http://localhost:8002/"))
+//                .build();
+//
+//    }
 }
